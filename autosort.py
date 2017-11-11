1bea075b2fcac3622ac266aff3d2901ac88953b4
Maarten de Vries <maarten@de-vri.es>
2017-11-11 15:05:25 +0100

    Bump version in script to 3.1.

--- autosort.py
+++ autosort.py
@@ -65,7 +65,7 @@ import weechat
 
 SCRIPT_NAME     = 'autosort'
 SCRIPT_AUTHOR   = 'Maarten de Vries <maarten@de-vri.es>'
-SCRIPT_VERSION  = '3.0'
+SCRIPT_VERSION  = '3.1'
 SCRIPT_LICENSE  = 'GPL3'
 SCRIPT_DESC     = 'Flexible automatic (or manual) buffer sorting based on eval expressions.'
 
